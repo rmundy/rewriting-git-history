6348a7a428b97f52238e614d897857425348a874
Brad Bow <brad.bow@live.com>
2018-04-22 17:36:45 +1000

    tests for abilities service

--- src/app/abilities.service.spec.ts
+++ src/app/abilities.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { AbilitiesService } from './abilities.service';
+import { Hero } from './hero';
 
 describe('AbilitiesService', () => {
   beforeEach(() => {
@@ -11,5 +12,16 @@ describe('AbilitiesService', () => {
 
   it('should be created', inject([AbilitiesService], (service: AbilitiesService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should return abilities', inject([AbilitiesService], (service: AbilitiesService) => {
+    expect(service.getAvailableAbilities().length).toBeGreaterThan(0);
+  }));
+
+  it('should set ability for hero', inject([AbilitiesService], (service: AbilitiesService) => {
+    let hero: Hero = { id: 1, name: 'spider boy'};
+    let abilities: String[] = ['speed', 'flight']
+    expect(() => service.setAbilitiesForHero(hero, abilities)).not.toThrow();
+    expect(service.getAbilitiesForHero(hero)).toBe(abilities);
   }));
 });
