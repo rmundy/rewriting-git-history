c6f90ed38648f45bb7b1f51aa0142781c80fbb39
Brad Bow <brad.bow@live.com>
2018-05-05 18:56:12 +1000

    add service for managing hero abilities

---
+++ src/app/abilities.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { difference } from 'lodash';
+
+@Injectable()
+export class AbilitiesService {
+
+  availableAbilities: String[] = [
+    'speed',
+    'strength',
+    'x-ray vision',
+    'flight',
+    'mind control',
+    'shape shifting',
+    'telekinesis'
+  ];
+
+  heroAbilities = {
+
+  };
+
+  public getAvailableAbilities = () => this.availableAbilities;
+
+  public setAbilitiesForHero = (hero: Hero, abilities: String[]) => {
+
+    if (this.testForIllegalAbilities(abilities)) {
+      throw Error('illegal abilities');
+    }
+
+    return this.heroAbilities[hero.id] = abilities;
+  }
+
+  public getAbilitiesForHero = (hero: Hero) =>
+    this.heroAbilities[hero.id] ? this.heroAbilities[hero.id] : [];
+
+  private testForIllegalAbilities = (abilities: String[]) => abilities
+    .map(i => this.availableAbilities.indexOf(i) < 0)
+    .reduce((acc, next) => acc || next, false);
+}
